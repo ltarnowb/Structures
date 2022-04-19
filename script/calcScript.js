b9e8a0c44490ee2fe3988da25fd2c7a07f9b36cd
ltarnowb <ltarnowb@gmail.com>
2022-04-19 13:53:13 -0400

    implementJson

---
+++ script/calcScript.js
@@ -0,0 +1,15 @@
+let dropdown = $('#shape-drop');
+
+dropdown.empty();
+
+dropdown.append('<option selected="true" disabled>Choose Shape</option>');
+dropdown.prop('selectedIndex', 0);
+
+const url = "\Structures\data\steel\Angle REF.json";
+
+// Populate dropdown with list of provinces
+$.getJSON(url, function (data) {
+  $.each(data, function (key, entry) {
+    dropdown.append($('<option></option>').attr('value', entry.abbreviation).text(entry.name));
+  })
+});
